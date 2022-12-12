e230b9251fdf8cdabfdb3ccab0f6041685a50a46
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-12-12 13:02:26 +0100

    ***82*** -->primera parte del diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { GiftsModule } from './gifts/gifts.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, GiftsModule],
 })
-export class AppModule { }
+export class AppModule {}
